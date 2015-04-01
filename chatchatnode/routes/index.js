b80798fd993acef2c833212285481d38f6b44083
Ubuntu <ubuntu@ip-172-31-38-28.us-west-2.compute.internal>
2015-04-01 08:52:56 +0000

    MongoDB connection established, added sign in, check account availability, etc

--- chatchatnode/routes/index.js
+++ chatchatnode/routes/index.js
@@ -6,14 +6,59 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/account', function(req, res) {
+// create a new account, assuming no repetetion of id (checked already)
+// accepts: id, password
+router.get('/account', function(req, res, next) {
     var db = req.db;
     var collection = db.get('account');
-    collection.insert({
-	id: 'asdfsadf',
-	password: '123213'
-    }, function (err, doc) {if (err) throw err}); 
-    res.render('account', {}); 
+    var aid = req.query['id'];
+    var apassword = req.query['password'];
+    
+    collection.insert({id: aid, password: apassword}, function(err, doc) {
+	if (err){ res.send('yes');} else {res.send('no');}
+    });     
+});
+
+// check account id existence
+router.get('/check_account', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('account');
+    var aid = req.query['id'];
+    
+    collection.find({id: aid}, {}, function(err, docs) {
+	if (docs.length > 0) {
+	    res.send('no');
+	} else {
+	    res.send('yes');
+	}
+    });
+});
+
+// login
+router.get('/login', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('account');
+    var aid = req.query['id'];
+    var apassword = req.query['password'];
+
+    collection.find({id:aid, password:apassword}, {}, function(err, docs) {
+	if (docs.length == 1) {
+	    res.send('yes');
+	} else {
+	    res.send('no');
+	}
+    });
+});
+
+// get contact info
+router.get('/get_contacts', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('contact');
+    var aid = req.query['id'];
+
+    collection.find({id: aid}, {}, function(err, docs) {
+	res.json(docs);
+    });
 });
 
 module.exports = router;
